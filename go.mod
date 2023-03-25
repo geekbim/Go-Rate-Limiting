797751744c5e73f3bdb9d78736e209bb3541fd58
Abim <abim@Abims-MacBook-Pro.local>
2023-03-25 22:02:52 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/geekbim/Go-Rate-Limiting
+
+go 1.20
+
+require (
+	github.com/didip/tollbooth/v7 v7.0.1 // indirect
+	github.com/go-pkgz/expirable-cache v0.1.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/didip/tollbooth/v7"
+	"golang.org/x/time/rate"
+)
+
+type Message struct {
+	Status string `json:"status"`
+	Body   string `json:"body"`
+}
+
+func endpointHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	message := Message{
+		Status: "Success",
+		Body:   "Hi! You've reached the API. How may I help you?",
+	}
+	err := json.NewEncoder(writer).Encode(&message)
+	if err != nil {
+		return
+	}
+}
+
+func rateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			message := Message{
+				Status: "Failed",
+				Body:   "The API is at capacity, try again later.",
+			}
+
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(&message)
+			return
+		} else {
+			next(w, r)
+		}
+	})
+}
+
+func perClientRateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+		clients[ip].lastSeen = time.Now()
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+
+			message := Message{
+				Status: "Failed",
+				Body:   "The API is at capacity, try again later.",
+			}
+
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(&message)
+			return
+		}
+		mu.Unlock()
+		next(w, r)
+	})
+}
+
+func main() {
+	http.Handle("/test1", rateLimiter(endpointHandler))
+	http.Handle("/test2", perClientRateLimiter(endpointHandler))
+
+	message := Message{
+		Status: "Failed",
+		Body:   "The API is at capacity, try again later.",
+	}
+	jsonMessage, _ := json.Marshal(message)
+
+	tlbthLimiter := tollbooth.NewLimiter(1, nil)
+	tlbthLimiter.SetMessageContentType("application/json")
+	tlbthLimiter.SetMessage(string(jsonMessage))
+
+	http.Handle("/test3", tollbooth.LimitFuncHandler(tlbthLimiter, endpointHandler))
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Println("There was an error listening on port :8080", err)
+	}
+}
